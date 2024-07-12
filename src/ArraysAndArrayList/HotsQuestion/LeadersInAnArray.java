87acf1cd4e5a0ff4206d9038a0dbebfda6aca12d
RahulSDE <baaburahulsingh@gmail.com>
2024-07-12 14:22:19 +0530

    questionspracticed

---
+++ src/ArraysAndArrayList/HotsQuestion/LeadersInAnArray.java
@@ -0,0 +1,34 @@
+package ArraysAndArrayList.HotsQuestion;
+
+public class LeadersInAnArray {
+    public static boolean greaterThanAllRight(int[] arr, int i){
+        /* ****************Brute-Force approach ******************** */
+        int j = arr.length-1;
+        while(j>=i){
+            if(arr[j]>arr[i]) return false; // larger no. is present on the right side of i
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,7,1,0};
+        int n = arr.length;
+        int i = n-1;
+//        while(i>=0){
+//            // checking whether the number right to the arr[i] is smaller than arr[i] or not.
+//            boolean bool = greaterThanAllRight(arr,i);
+//            if(bool) System.out.print(arr[i] + " ");
+//            i--;
+//        }
+
+        /* *********Optimal Approach ********************** */
+        int largest = Integer.MIN_VALUE;
+        while(i>=0){
+            if(arr[i]>largest){
+                largest = arr[i];
+                System.out.print(arr[i] + " ");
+            }
+            i--;
+        }
+    }
+}
---
+++ src/ArraysAndArrayList/HotsQuestion/LongestConsecutiveSequence.java
@@ -0,0 +1,47 @@
+package ArraysAndArrayList.HotsQuestion;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence {
+    public static int lengthofConsecutiveSequence(int[] arr){
+        int lastSmallest = Integer.MIN_VALUE;
+        int maxLength = 0;
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-1==lastSmallest){
+                count++;
+            }
+            else if(arr[i]-1 != lastSmallest){
+                count = 1;
+            }
+            lastSmallest =arr[i];
+            maxLength = Math.max(count,maxLength);
+        }
+        return maxLength;
+    }
+    public static boolean linearSearch(int[] nums, int x){
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==x) return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {102,4,100,1,101,3,2,1,1};
+        int n = arr.length;
+        int maxLength = Integer.MIN_VALUE;
+        /* ****************BruteForce Approach **************/
+//        for(int i=0;i<n;i++){
+//            int x = arr[i];
+//            int count = 1;
+//            while(linearSearch(arr, x + 1)){
+//                count++;
+//                x += 1;
+//            }
+//            maxLength = Math.max(count,maxLength);
+//        }
+        Arrays.sort(arr);
+        maxLength = lengthofConsecutiveSequence(arr);
+        System.out.println("The length of the longest consecutive sequence is: " + maxLength);
+    }
+}
