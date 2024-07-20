ee0f1ef07d9ae6e34daa11a6295f7ceb5aa7a2c9
RahulSDE <baaburahulsingh@gmail.com>
2024-07-20 17:23:33 +0530

    recursion

---
+++ src/Recursion/ClassWork/StringTraversal.java
@@ -0,0 +1,14 @@
+package Recursion.ClassWork;
+
+public class StringTraversal {
+    public static void print(int i, String s){
+        if(i==s.length()) return;
+        System.out.print(s.charAt(i)+"");
+        print(i+1,s);
+    }
+    public static void main(String[] args) {
+        String s = "Rahul";
+        //System.out.println(s);
+        print(0,s);
+    }
+}
