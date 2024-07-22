20553e7b1d1c8009d9bd0060fb2b4ec10de7c9c4
RahulSDE <baaburahulsingh@gmail.com>
2024-07-22 20:22:46 +0530

    recursion

---
+++ src/Recursion/ClassWork/SubsetOfString.java
@@ -0,0 +1,19 @@
+package Recursion.ClassWork;
+import java.util.ArrayList;
+public class SubsetOfString {
+    static ArrayList<String> ans = new ArrayList<>();
+    public static void subset(int i,String s,String t){
+        if(i==s.length()){
+            ans.add(t);
+            return;
+        }
+        char ch = s.charAt(i);
+        subset(i+1,s,t);
+        subset(i+1,s,t+ch);
+    }
+    public static void main(String[] args) {
+        String s = "123";
+        subset(0,s,"");
+        System.out.println(ans);
+    }
+}
