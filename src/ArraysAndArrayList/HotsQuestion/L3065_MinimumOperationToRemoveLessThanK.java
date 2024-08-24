6e30ab2cced0268886960a23254353bbc34cb16a
RahulSDE <baaburahulsingh@gmail.com>
2024-08-24 10:56:25 +0530

    modification on minimumOperationToRemoveLessThanK file

--- src/ArraysAndArrayList/HotsQuestion/L3065_MinimumOperationToRemoveLessThanK.java
+++ src/ArraysAndArrayList/HotsQuestion/L3065_MinimumOperationToRemoveLessThanK.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class L3065_MinimumOperationToRemoveLessThanK {
     public static int minOperations(int[] nums, int k) {
         // traversing the array to check how many elements in the array < k
-        int count = 0;
+        int count = 0; // will keep the count of no <k
         for(int i=0;i<nums.length;i++){
             if(nums[i]<k) count++;
         }
@@ -12,7 +12,8 @@ public class L3065_MinimumOperationToRemoveLessThanK {
 
     public static void main(String[] args) {
         int[] nums = {10,11,12,1,2,3};
-        int min =  minOperations(nums,10);
+        int min =  minOperations(nums,10); // function call
         System.out.println(min);
+
     }
 }
