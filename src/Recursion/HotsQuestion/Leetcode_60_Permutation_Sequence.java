5b5f141f1d2ba59c25658f346242014832e62ca5
RahulSDE <baaburahulsingh@gmail.com>
2024-08-29 13:37:18 +0530

    optimized solution of permutation sequence

--- src/Recursion/HotsQuestion/Leetcode_60_Permutation_Sequence.java
+++ src/Recursion/HotsQuestion/Leetcode_60_Permutation_Sequence.java
@@ -33,11 +33,31 @@ public class Leetcode_60_Permutation_Sequence {
 //    }
 
     // ***************** Approach-2 ***********************
-    
+    public static String getPermutation1(int n, int k) {
+        int fact = 1;
+        // calculating total permutation possible with each number of arrayList numbers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i=1;i<n;i++){
+            fact = fact * i;
+            numbers.add(i);
+        }
+        k = k -1;
+        numbers.add(n);
+        String ans = ""; // will store the final result
+        while(true){
+            ans = ans + numbers.get(k/fact);
+            numbers.remove(k/fact);
+            if(numbers.isEmpty()) break;
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int n = 3;
         int k = 3;
-        String ans = getPermutation(n,k);
+        String ans = getPermutation1(n,k);
         System.out.println(ans);
         // 123 132 213 231 312 321
     }
