7ffa4bfc31f80248d350ca6c46fae4a2f516417d
RahulSDE <baaburahulsingh@gmail.com>
2024-09-05 12:22:58 +0530

    practice

---
+++ src/OOps/EmployeeDetails.java
@@ -0,0 +1,38 @@
+package OOps;
+import java.util.*;
+class Employee{
+    private int eid;
+    private String ename;
+    private int eage;
+    public void setEid(int eid){
+        this.eid = eid;
+    }
+    public void setEname(String ename){
+        this.ename = ename;
+    }
+    public void setEage(int eage){
+        this.eage = eage;
+    }
+
+    public int getEid(){
+        return eid;
+    }
+    public String getEname(){
+        this.ename = ename;
+    }
+    public int getEage(){
+        this.eage = eage;
+    }
+}
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.setEage(18);
+        e1.setEid(123);
+        e1.setEname("rahul");
+
+        System.out.println("name is:: " + e1.getEname());
+        System.out.println("id is:: " + e1.getEid());
+        System.out.println("age is:: " + e1.getEage());
+    }
+}
