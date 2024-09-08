bcb5965a3502159fd5e11e19d5e325001170ae93
RahulSDE <baaburahulsingh@gmail.com>
2024-09-08 20:18:00 +0530

    oops concept

---
+++ src/OOps/Constructor.java
@@ -0,0 +1,21 @@
+package OOps;
+import java.util.*;
+class Employees{
+    String name;
+    int age;
+    // zero argument constructor
+    Employees(){
+        System.out.println("the constructor is being called......");
+    }
+    // instance method
+    public void disp(){
+        System.out.println("Name is :: "+name);
+        System.out.println("Age is  :: " +age);
+    }
+}
+public class Constructor {
+    public static void main(String[] args) {
+        Employees e1  = new Employees();
+        e1.disp();
+    }
+}
---
+++ src/OOps/Inheritance.java
@@ -0,0 +1,31 @@
+package OOps;
+import java.util.*;
+class Person{
+    public String name;
+    public int age;
+    public String address;
+}
+class Students extends Person{
+    public int marks;
+    public String grade;
+    Students(String name, int age, String address, int marks, String grade){
+        this.grade = grade;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+        this.address = address;
+    }
+    public void disp(){
+        System.out.println("Name is     :: "+name);
+        System.out.println("Age is      :: "+age);
+        System.out.println("Address is  :: "+address);
+        System.out.println("Marks is    :: "+marks);
+        System.out.println("grade is    :: "+grade);
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Students s1 = new Students("Rahul",19,"noida",49,"A+");
+        s1.disp();
+    }
+}
