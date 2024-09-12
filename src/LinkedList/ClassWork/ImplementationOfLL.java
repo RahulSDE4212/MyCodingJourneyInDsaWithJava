0774b0d4cf6fa870fd85ebdc9db25d394c2d20c6
RahulSDE <baaburahulsingh@gmail.com>
2024-09-12 13:57:56 +0530

    linked list updated

--- src/LinkedList/ClassWork/ImplementationOfLL.java
+++ src/LinkedList/ClassWork/ImplementationOfLL.java
@@ -1,10 +1,10 @@
 package LinkedList.ClassWork;
 // creating our own data structure name SinglyLinkedList
 class SLL{
-    Node head; // which will refer to the starting node of Linked list
-    Node tail; // which will refer to the ending node of linked list
-    int size;
-    void add(int val){ // function to add node in the linked list
+    private Node head; // which will refer to the starting node of Linked list
+    private Node tail; // which will refer to the ending node of linked list
+    private int size;
+    void insertAtTail(int val){ // function to add node in the linked list
         Node temp = new Node(val);
         if(head==null) head = tail = temp;
         else{
@@ -24,19 +24,26 @@ class SLL{
     void size(){
         System.out.println("The length of the linked list : " + size);
     }
+    void insetAtHead(int val){
+        Node temp = new Node(val);
+        if(head == null) head = tail = temp;
+        else{
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
 }
 public class ImplementationOfLL {
     public static void main(String[] args) {
         SLL list = new SLL();
-        list.add(10);
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
         list.display();
-        list.size();
-        list.add(20);
+        list.insertAtTail(40);
         list.display();
-        list.size();
-        list.add(30);
-        list.display();
-        list.add(40);
+        list.insetAtHead(50);
         list.display();
     }
 }
