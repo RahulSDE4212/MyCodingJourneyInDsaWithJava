333716035818e70a88807b64aad8401e234b5326
RahulSDE <baaburahulsingh@gmail.com>
2024-09-12 16:40:49 +0530

    implementation of java file updated with insert function which will insert the node at any given index

--- src/LinkedList/ClassWork/ImplementationOfLL.java
+++ src/LinkedList/ClassWork/ImplementationOfLL.java
@@ -24,7 +24,7 @@ class SLL{
     void size(){
         System.out.println("The length of the linked list : " + size);
     }
-    void insetAtHead(int val){
+    void insertAtHead(int val){
         Node temp = new Node(val);
         if(head == null) head = tail = temp;
         else{
@@ -33,6 +33,29 @@ class SLL{
         }
         size++;
     }
+    void insert(int idx, int val){
+        Node temp = new Node(val);
+        Node x = head;
+        if(idx == 0) {
+            insertAtHead(val);
+            return;
+        }
+        if(idx == size){
+            insertAtTail(val);
+            return;
+        }
+        if(idx>size){
+            System.out.println("Invalid index ..... ");
+            return;
+        }
+        // i need to traverse the linked list till index - 1
+        // so i need to run the loop idx-1 times
+        for(int i=0;i<idx-1;i++){
+            x = x.next;
+        }
+        temp.next = x.next;
+        x.next = temp;
+    }
 }
 public class ImplementationOfLL {
     public static void main(String[] args) {
@@ -40,10 +63,18 @@ public class ImplementationOfLL {
         list.insertAtTail(10);
         list.insertAtTail(20);
         list.insertAtTail(30);
-        list.display();
+        //list.display();
         list.insertAtTail(40);
+        //.display();
+        list.insertAtHead(50);
         list.display();
-        list.insetAtHead(50);
+        list.insert(2,100);
+        list.display();
+        list.insert(6,80);
+        list.display();
+        list.insert(0,2);
+        list.display();
+        list.insert(10,45);
         list.display();
     }
 }
