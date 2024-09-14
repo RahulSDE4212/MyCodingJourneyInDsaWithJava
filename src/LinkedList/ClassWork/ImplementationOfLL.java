3a216f0dbfb6666dd2cfd836351a372f56277df6
RahulSDE <baaburahulsingh@gmail.com>
2024-09-14 17:36:28 +0530

    linked list folder update

--- src/LinkedList/ClassWork/ImplementationOfLL.java
+++ src/LinkedList/ClassWork/ImplementationOfLL.java
@@ -2,17 +2,8 @@ package LinkedList.ClassWork;
 // creating our own data structure name SinglyLinkedList
 class SLL{
     private Node head; // which will refer to the starting node of Linked list
-    private Node tail; // which will refer to the ending node of linked list
+    public Node tail; // which will refer to the ending node of linked list
     private int size;
-    void insertAtTail(int val){ // function to add node in the linked list
-        Node temp = new Node(val);
-        if(head==null) head = tail = temp;
-        else{
-            tail.next = temp;
-            tail = temp;
-        }
-        size++;
-    }
     void display(){ // function to display the node of the linked list
         Node temp = head;
         while(temp!= null) {
@@ -55,6 +46,16 @@ class SLL{
         }
         temp.next = x.next;
         x.next = temp;
+        size++;
+    }
+    void insertAtTail(int val){ // function to add node in the linked list
+        Node temp = new Node(val);
+        if(head==null) head = tail = temp;
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
     }
     int get(int idx) throws Error{
         if(idx<0 || idx>=size) throw new Error("bhai error aa gya!!!!");
@@ -76,6 +77,26 @@ class SLL{
             temp = temp.next;
         }
         temp.val = val;
+    }
+    void deleteAtHead() throws Error{
+        if(head==null){
+            throw new Error("kya kar rha hai bhai khali linked list me se kya hi hatau!!!!!!1 ");
+        }
+        head = head.next;
+        size--;
+    }
+    void delete(int idx) throws Error{
+        if(idx==0) deleteAtHead();
+        if(idx<0 || idx>=size) throw new Error("kya kar rha hai bhai kuchh to dimal laga, idx ka limit cross mat kar!!!!!!!!");
+        Node temp = head;
+        // traverse till idx - 1;
+        for(int i=1;i<=idx-1;i++){
+            temp = temp.next;
+        }
+        // now deleting the node at idx
+        if(temp.next == tail) tail = temp;
+        temp.next = temp.next.next;
+        size--;
     }
 }
 public class ImplementationOfLL {
@@ -100,5 +121,16 @@ public class ImplementationOfLL {
         System.out.println(list.get(4));
         list.set(3,8);
         list.display();
+        list.size();
+        list.deleteAtHead();
+        list.display();
+        list.size();
+        list.delete(3);
+        list.display();
+        list.size();
+        list.delete(5);
+        list.display();
+        list.size();
+        System.out.println(list.tail.val);
     }
 }
