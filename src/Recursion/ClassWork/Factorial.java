0f3a624050867f136d95248f70b90d4a65f1ea40
RahulSDE <baaburahulsingh@gmail.com>
2024-09-16 13:26:20 +0530

    recursion practice

--- src/Recursion/ClassWork/Factorial.java
+++ src/Recursion/ClassWork/Factorial.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class Factorial {
     public static int fact(int n){
+        // fact(n) says that if n==1 then i know my exact answer it is 1 only
+        // base case
         if(n==1) return 1;
-        return n * fact(n-1);
+        // fact(n) is saying that i don't know the exact answer
+        // please ask fact(n-1) first what is his answer
+        // then i will be able to know the exact answer
+        // because i know that my answer is n * fact(n-1)
+        int ans = n * fact(n-1);
+        return ans;
     }
     public static void main(String[] args) {
-        System.out.println("Enter the number for factorial: ");
+        System.out.println("Enter the n: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(fact(n));
+        // calling the function fact whose task is to calculate factorial of n
+        int ans = fact(n);
+        System.out.println(ans);
     }
 }
--- src/Recursion/ClassWork/PrintNTo1.java
+++ src/Recursion/ClassWork/PrintNTo1.java
@@ -3,15 +3,21 @@ package Recursion.ClassWork;
 import java.util.Scanner;
 
 public class PrintNTo1 {
-    public static void printing(int n){
-        if(n==0) return;
-        System.out.print(n + " ");
-        printing(n-1);
+    public static void print(int n){
+        // print(n) says that if n<1 please just return do not need to do anything
+        // base case
+        if(n<1) return;
+        // print(n) says that i don't need to do the entire task
+        // what i will do is that i will print n only
+        // please ask print(n-1) to print 1 to n-1
+        System.out.println(n);
+        print(n-1);
     }
     public static void main(String[] args) {
-        System.out.println("Enter n: ");
+        System.out.println("Enter the n: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printing(n);
+        // calling the method print(n) whose task is to print 1 to n
+        print(n);
     }
 }
