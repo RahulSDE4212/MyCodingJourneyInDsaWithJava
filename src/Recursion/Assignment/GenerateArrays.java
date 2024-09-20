f62eb0886687c2a777f2f82bf40cfd51dda8032a
RahulSDE <baaburahulsingh@gmail.com>
2024-09-20 19:36:16 +0530

    recrusion assignment

--- src/Recursion/Assignment/GenerateArrays.java
+++ src/Recursion/Assignment/GenerateArrays.java
@@ -2,54 +2,38 @@ package Recursion.Assignment;
 import java.util.ArrayList;
 import java.util.List;
 public class GenerateArrays {
-    // Function to generate all possible arrays
-    public static void generateUtil(int[] A, int[] B, List<Integer> currentArray, int i, int j, boolean flag) {
-        // If the current array ends with an element from B, print it
-        if (currentArray.size()>0 && currentArray.size() % 2 == 0) {
-            System.out.println(currentArray);
+    // creating a method which will generate all the required arrays whose element is in
+    // increasing order such that first element is from array A and second element is from array B
+    // and this alternating fashion is continued till the last element of array B
+    public static void generateAllArrays(int[] A, int[] B, List<Integer> ans, int i, int j,boolean bool){
+        // base case
+        if(!ans.isEmpty() && ans.size()%2==0){
+            System.out.println(new ArrayList<>(ans));
         }
-
-        // If the turn is to pick from array A
-        if (flag) {
-            // Choose each element from A starting from index i and append to currentArray
-            for (int k = i; k < A.length; k++) {
-                if (currentArray.size() == 0 || A[k] > currentArray.get(currentArray.size() - 1)) {
-                    currentArray.add(A[k]);
-                    // Recursion call to pick next element from B
-                    generateUtil(A,B, currentArray,k+1,j, !flag);
-                    // Backtrack by removing the last element
-                    currentArray.remove(currentArray.size() - 1);
+        if(bool){
+            for(int k=i;k<A.length;k++){
+                if(ans.size()==0 || A[k]>ans.get(ans.size()-1)) {
+                    ans.add(A[k]);
+                    generateAllArrays(A,B,ans,k+1,j,!bool);
+                    if(!ans.isEmpty()) ans.remove(ans.size()-1);
                 }
             }
         }
-        // If the turn is to pick from array B
-        else {
-            for (int k = j; k < B.length; k++) {
-                if (B[k] > currentArray.get(currentArray.size() - 1)) {
-                    currentArray.add(B[k]);
-                    // Recursion call to pick next element from A
-                    generateUtil(A, B, currentArray,i, k + 1, !flag);
-                    // Backtrack by removing the last element
-                    currentArray.remove(currentArray.size() - 1);
+        else{
+            for(int k=j;k<B.length;k++){
+                if(B[k]>ans.get(ans.size()-1)) {
+                    ans.add(B[k]);
+                    generateAllArrays(A,B,ans,i,k+1,!bool);
+                    ans.remove(ans.size()-1);
                 }
+
             }
         }
     }
-
-    // Helper function to start generating arrays
-    public static void generateAllArrays(int[] A, int[] B) {
-        // Create an empty list to store current combination
-        List<Integer> currentArray = new ArrayList<>();
-        // Call recursive function starting from array A
-        generateUtil(A, B, currentArray, 0, 0, true);
-    }
-
     public static void main(String[] args) {
-        // Sample arrays
         int[] A = {10, 15, 25};
         int[] B = {1, 17, 20, 30};
-
-        // Generate all possible arrays
-        generateAllArrays(A, B);
+        // calling generate all arrays function
+        generateAllArrays(A,B,new ArrayList<>(),0,0,true);
     }
 }
