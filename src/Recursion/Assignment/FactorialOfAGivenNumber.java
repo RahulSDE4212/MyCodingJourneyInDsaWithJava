233bb8e4bcfa25da225c8b8838f6d8f9477b4577
RahulSDE <baaburahulsingh@gmail.com>
2024-09-21 16:24:04 +0530

    recursion practice

---
+++ src/Recursion/Assignment/FactorialOfAGivenNumber.java
@@ -0,0 +1,15 @@
+package Recursion.Assignment;
+import java.util.*;
+public class FactorialOfAGivenNumber {
+    public static int factorial(int n){
+        if(n==1) return 1;
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println("enter n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        System.out.println("The factorial of " + n + " is: " + fact);
+    }
+}
--- src/Recursion/Assignment/Leetcode_90_subsetII.java
+++ src/Recursion/Assignment/Leetcode_90_subsetII.java
@@ -1,8 +1,20 @@
 package Recursion.Assignment;
 import java.util.*;
 public class Leetcode_90_subsetII {
+    static List<List<Integer>> finalAns = new ArrayList<>();
+    public static void subset(int[] nums,List<Integer> ans, int idx){
+        finalAns.add(new ArrayList<>(ans));
+        for(int i=idx;i<nums.length;i++){
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            ans.add(nums[i]);
+            subset(nums,ans,i+1);
+            ans.remove(ans.size()-1);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,2};
-        subset(arr);
+        Arrays.sort(arr);
+        subset(arr,new ArrayList<>(),0);
+        System.out.println(finalAns);
     }
 }
