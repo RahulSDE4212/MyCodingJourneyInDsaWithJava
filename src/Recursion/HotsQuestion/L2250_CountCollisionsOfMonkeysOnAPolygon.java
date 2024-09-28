aa65621167967470ebbce2fd12c3c6f4ff1afb16
RahulSDE <baaburahulsingh@gmail.com>
2024-09-29 01:01:36 +0530

    recursion practice

---
+++ src/Recursion/HotsQuestion/L2250_CountCollisionsOfMonkeysOnAPolygon.java
@@ -0,0 +1,24 @@
+package Recursion.HotsQuestion;
+
+public class L2250_CountCollisionsOfMonkeysOnAPolygon {
+    public static int mod = 1000000007;
+    public static long countCollisions(int n){
+        long noOfMovement = pow(n);
+        long ans = (pow(n)%mod-2)%mod;
+        return ans;
+    }
+    public static long pow(int n){
+        // this method is created to count the no. of ways monkeys can move.
+        if(n==0) return 2;
+        long ans = pow(n/2);
+        if(n%2==0) {
+            return (ans * ans)%mod;
+        }
+        else return ((ans*ans)%mod*2)%mod;
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        long ans = countCollisions(n);
+        System.out.println("The no. of collisons are: " + ans);
+    }
+}
---
+++ src/Recursion/HotsQuestion/Pow_x_n.java
@@ -0,0 +1,26 @@
+package Recursion.HotsQuestion;
+
+public class Pow_x_n {
+    public static double helper(double x, int n){
+            if(n==0) return 1;
+            double ans = helper(x,n/2);
+            if(n%2==0) return ans * ans;
+            else return ans * ans * x;
+    }
+    public static double myPow(double x, int n) {
+        boolean bool = false;
+        if(n<0){
+            n = n * -1;
+            bool = true;
+        }
+        double ans = helper(x,n);
+        if(bool) ans = 1 / ans;
+        return ans;
+    }
+    public static void main(String[] args) {
+        double x = 2.000000;
+        int n = -2;
+        double ans = myPow(x,n);
+        System.out.println(ans);
+    }
+}
