75f63f2a797debf457a4c7270221309a84467d34
RahulSDE <baaburahulsingh@gmail.com>
2024-09-30 21:47:07 +0530

    recursion

---
+++ src/Recursion/HotsQuestion/L390_EliminationGame.java
@@ -0,0 +1,9 @@
+package Recursion.HotsQuestion;
+import java.util.*;
+public class L390_EliminationGame {
+    public static void main(String[] args) {
+        int n = 9;
+        //int last = lastRemaining(n);
+        //System.out.println(last);
+    }
+}
