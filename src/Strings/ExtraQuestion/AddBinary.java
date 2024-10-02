4dd257c3ddd51573559dc47efd0a1d726ddc22a8
RahulSDE <baaburahulsingh@gmail.com>
2024-10-02 19:40:48 +0530

    string questions

---
+++ src/Strings/ExtraQuestion/AddBinary.java
@@ -0,0 +1,30 @@
+package Strings.ExtraQuestion;
+
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        while(i>=0 || j>=0 || carry==1){
+            int sum = carry;
+            if(i>=0){
+                sum += a.charAt(i)-'0';
+                i--;
+            }
+            if(j>=0){
+                sum += b.charAt(j) -'0';
+                j--;
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        String s = "101011";
+        String t = "111";
+        String r = addBinary(s,t);
+        System.out.println(r);
+    }
+}
