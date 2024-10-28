e4d5b15389b853ee986218c51bedabb49de5b675
RahulSDE <baaburahulsingh@gmail.com>
2024-10-28 23:52:45 +0530

    another approach

--- src/LinkedList/ClassWork/PalindromeLinkedList.java
+++ src/LinkedList/ClassWork/PalindromeLinkedList.java
@@ -16,15 +16,47 @@ public class PalindromeLinkedList {
         }
         return true;
     }
+    public static boolean PalindromeUsingLLDeepCopy(Node head){
+        Node head2 = new Node(head.val);
+        Node temp2 = head2;
+        Node temp1 = head.next;
+        while(temp1!=null){
+            Node aux = new Node(temp1.val);
+            temp2.next = aux;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        // reversing the linked list head2
+        Node pre = null;
+        Node curr = head2;
+        Node next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        head2 = pre;
+
+        temp1 = head;
+        temp2 = head2;
+        while(temp1!=null){
+            if(temp1.val != temp2.val) return false;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         SLL list = new SLL();
         list.insertAtTail(10);
         list.insertAtTail(20);
         list.insertAtTail(30);
+        list.insertAtTail(40);
         list.insertAtTail(20);
         list.insertAtTail(10);
         list.display();
-        boolean bool = isPalindromeUsingArrayList(list.head);
+        boolean bool = PalindromeUsingLLDeepCopy(list.head);
         System.out.println(bool);
     }
 }
