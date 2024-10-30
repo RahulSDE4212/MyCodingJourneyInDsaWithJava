355911cc786a8eff3e00a2986f8f81b116a732a5
RahulSDE <baaburahulsingh@gmail.com>
2024-10-31 00:28:33 +0530

    leetcode practice

--- src/Strings/ExtraQuestion/L415_AddString.java
+++ src/Strings/ExtraQuestion/L415_AddString.java
@@ -32,6 +32,22 @@ public class L415_AddString {
         if(carry!=0) sb.insert(0,carry);
         return sb.toString();
     }
+    public static  String addStrings2(String num1, String num2) { // optimized approach
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i>=0 || j>=0 || carry!=0){
+            int n1 = (i>=0) ? num1.charAt(i)-'0':0;
+            int n2 = (j>=0) ? num2.charAt(j)-'0':0;
+            int sum = n1 + n2 + carry;
+            sb.append(sum%10);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
     public static void main(String[] args) {
         String num1 = "123";
         String num2 = "23456";
