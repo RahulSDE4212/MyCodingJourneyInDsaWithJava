0f10d3eb25e615b803da067c92b1ae7171cfc1d8
RahulSDE <baaburahulsingh@gmail.com>
2024-12-20 01:14:20 +0530

    conversion of prefix and postfix

---
+++ src/Stack/ClassWork/InfixPrefixPostfix/ConversionOfPostfixToInfix.java
@@ -0,0 +1,24 @@
+package Stack.ClassWork.InfixPrefixPostfix;
+
+import java.util.Stack;
+
+public class ConversionOfPostfixToInfix {
+    public static void main(String[] args) {
+        String s = "953+4*6/-";
+        Stack<String> res = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                res.push("" + ch);
+            }
+            else{
+                String v2 = res.pop();
+                String v1 = res.pop();
+                String ans = '(' + v1 + ch +  v2 + ')';
+                res.push(ans);
+            }
+        }
+        System.out.println(res.peek());
+    }
+}
---
+++ src/Stack/ClassWork/InfixPrefixPostfix/ConversionOfPostfixToPrefix.java
@@ -0,0 +1,24 @@
+package Stack.ClassWork.InfixPrefixPostfix;
+
+import java.util.Stack;
+
+public class ConversionOfPostfixToPrefix {
+    public static void main(String[] args) {
+        String s = "953+4*6/-";
+        Stack<String> res = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                res.push("" + ch);
+            }
+            else{
+                String v2 = res.pop();
+                String v1 = res.pop();
+                String ans = ch + v1 + v2;
+                res.push(ans);
+            }
+        }
+        System.out.println(res.peek());
+    }
+}
