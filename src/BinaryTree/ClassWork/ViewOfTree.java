f994f809143d55bf8895678e9957c3b2f333f530
RahulSDE <baaburahulsingh@gmail.com>
2025-01-29 14:38:24 +0530

    dsa practice -- view of the tree and many more

--- src/BinaryTree/ClassWork/ViewOfTree.java
+++ src/BinaryTree/ClassWork/ViewOfTree.java
@@ -1,5 +1,13 @@
 package BinaryTree.ClassWork;
 import java.util.*;
+class Pair{
+    TreeNode node;
+    int level;
+    Pair(TreeNode node, int level){
+        this.node = node;
+        this.level = level;
+    }
+}
 public class ViewOfTree {
     public static int lvlOfTree(TreeNode root) {
         if(root == null) return 0;
@@ -17,12 +25,12 @@ public class ViewOfTree {
         dfs(root.left, level + 1, ans);
         dfs(root.right, level + 1, ans);
     }
-    /*   ******************* Right-Side View ******************
-        Approach: Depth First Search (DFS)
-        Time Complexity: O(N)
-        Space Complexity: O(H) where H is the height of the tree
-                               (due to recursion stack)
-     */
+
+    /*
+    Approach: Depth-First Search (DFS)
+    Time Complexity: O(N)
+    Space Complexity: O(H)
+    */
     public static List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         int level = lvlOfTree(root);
@@ -32,10 +40,12 @@ public class ViewOfTree {
         dfs(root, 0, ans);
         return ans;
     }
-    /* **************** Left-Side View *******************
-    * Time Complexity : O(n)
-    * Space Complexity: O(n)
-    * */
+
+    /*
+    Approach: Depth-First Search (DFS)
+    Time Complexity: O(N)
+    Space Complexity: O(H)
+    */
     public static ArrayList<Integer> leftView(TreeNode root) {
         // code here
         ArrayList<Integer> ans = new ArrayList<>();
@@ -47,7 +57,40 @@ public class ViewOfTree {
         dfs2(root, 0, ans);
         return ans;
     }
-    /* ************ Top-View *****************/
+
+    /*
+    Approach: Breadth-First Search (BFS)
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+    */
+    public static void topView(TreeNode root, HashMap<Integer, TreeNode> map){
+        Queue<Pair> q = new LinkedList<>();
+        if(root != null) q.add(new Pair(root, 0));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            TreeNode node = p.node;
+            if(!map.containsKey(p.level)) map.put(p.level, node);
+            if(node.left != null) q.add(new Pair(node.left, p.level - 1));
+            if(node.right != null) q.add(new Pair(node.right, p.level + 1));
+        }
+    }
+
+    /*
+    Approach: Breadth-First Search (BFS)
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+    */
+    public static void bottomView(TreeNode root, HashMap<Integer, TreeNode> map){
+        Queue<Pair> q = new LinkedList<>();
+        if(root != null) q.add(new Pair(root, 0));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            TreeNode node = p.node;
+            map.put(p.level, node);
+            if(node.left != null) q.add(new Pair(node.left, p.level - 1));
+            if(node.right != null) q.add(new Pair(node.right, p.level + 1));
+        }
+    }
 
     public static void main(String[] args) {
         TreeNode a = new TreeNode(2);
@@ -63,7 +106,23 @@ public class ViewOfTree {
         b.right = e;
         c.right = f;
         f.right = g;
-
+        List<Integer> rsv = rightSideView(a);
+        List<Integer> lsv = leftView(a);
+        HashMap<Integer, TreeNode> map = new HashMap<Integer, TreeNode>();
+        topView(a, map);
+        System.out.println("right side view of the tree: " + rsv);
+        System.out.println("left side view of the tree: " + lsv);
+        System.out.println("top view of the tree");
+        for(TreeNode node : map.values()){
+            System.out.print(node.val + " ");
+        }
+        System.out.println();
+        map.clear();
+        bottomView(a, map);
+        System.out.println("bottom view of the tree");
+        for(TreeNode node : map.values()){
+            System.out.print(node.val + " ");
+        }
 
     }
 }
